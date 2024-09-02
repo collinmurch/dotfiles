dd45dfaa0976fc8cbd18917a37d3d5ce5c744925
Collin Murch <collin@murch.net>
2024-09-01 17:02:33 -0700

    wezterm edits

--- .wezterm.lua
+++ .wezterm.lua
@@ -1,4 +1,4 @@
-local wezterm = require 'wezterm'
+local wezterm = require("wezterm")
 local act = wezterm.action
 local config = wezterm.config_builder()
 local hyperlink_rules = wezterm.default_hyperlink_rules()
@@ -10,21 +10,18 @@ local hyperlink_rules = wezterm.default_hyperlink_rules()
 -- You could also re-write this to match optimistically, then limit with lua, but the
 --    downside of that approach is a lot gets underlined but isn't clickable
 table.insert(hyperlink_rules, {
-  regex = "\\/([\\w.\\/\\-]*\\w+\\.\\w+)(:\\d+)?\\b",
-  format = "$0"
+	regex = "\\/([\\w.\\/\\-]*\\w+\\.\\w+)(:\\d+)?\\b",
+	format = "$0",
 })
 
 config = {
-	color_scheme = 'Catppuccin Mocha',
-	
-	font = wezterm.font(
-		'JetBrains Mono',
-		{weight = 'Medium'}
-	),
+	color_scheme = "Catppuccin Mocha",
+
+	font = wezterm.font("JetBrains Mono", { weight = "Medium" }),
 	font_size = 15.0,
 	window_background_opacity = 0.97,
 	macos_window_background_blur = 5,
-	
+
 	window_padding = {
 		left = 30,
 		right = 30,
@@ -32,94 +29,94 @@ config = {
 		bottom = 10,
 	},
 
-  -- Catppuccin Colors
+	-- Catppuccin Colors
 	window_frame = {
-		font = wezterm.font { family = 'JetBrains Mono', weight = 'Bold'},
+		font = wezterm.font({ family = "JetBrains Mono", weight = "Bold" }),
 		font_size = 13.0,
-		active_titlebar_bg = '#1E1E2E',
-		inactive_titlebar_bg = '#1E1E2E',
+		active_titlebar_bg = "#1E1E2E",
+		inactive_titlebar_bg = "#1E1E2E",
 	},
 	colors = {
 		tab_bar = {
 			active_tab = {
-				bg_color = '#1E1E2E',
-				fg_color = '#CDD6F4',
+				bg_color = "#1E1E2E",
+				fg_color = "#CDD6F4",
 			},
 			inactive_tab = {
-				bg_color = '313244',
-				fg_color = '#BAC2DE',
+				bg_color = "313244",
+				fg_color = "#BAC2DE",
 			},
 			new_tab = {
-				bg_color = '313244',
-				fg_color = '#BAC2DE',
+				bg_color = "313244",
+				fg_color = "#BAC2DE",
 			},
 			new_tab_hover = {
-				bg_color = '#1E1E2E',
-				fg_color = '#CDD6F4',
+				bg_color = "#1E1E2E",
+				fg_color = "#CDD6F4",
 			},
 		},
 	},
 
 	keys = {
 		-- Close the current pane instead of window if a pane is open
-		{ key = 'w', mods = 'CMD', action = act.CloseCurrentPane { confirm = false }},
+		{ key = "w", mods = "CMD", action = act.CloseCurrentPane({ confirm = false }) },
 
 		-- Remove the shift & ctrlselect printing random characters
-		{ key = 'LeftArrow', mods = 'SHIFT', action = act.Nop },
-		{ key = 'RightArrow', mods = 'SHIFT', action = act.Nop },
-		{ key = 'UpArrow', mods = 'SHIFT', action = act.Nop },
-		{ key = 'DownArrow', mods = 'SHIFT', action = act.Nop },
-    { key = 'UpArrow', mods = 'ALT', action = act.Nop },
-		{ key = 'DownArrow', mods = 'ALT', action = act.Nop },
-    
-    -- Word traversal
-    { key = 'LeftArrow', mods = 'ALT', action = wezterm.action.SendKey{ key = 'b', mods = 'ALT' } },
-    { key = 'RightArrow', mods = 'ALT', action = wezterm.action.SendKey{ key = 'f', mods = 'ALT' } },
+		{ key = "LeftArrow", mods = "SHIFT", action = act.Nop },
+		{ key = "RightArrow", mods = "SHIFT", action = act.Nop },
+		{ key = "UpArrow", mods = "SHIFT", action = act.Nop },
+		{ key = "DownArrow", mods = "SHIFT", action = act.Nop },
+		{ key = "UpArrow", mods = "ALT", action = act.Nop },
+		{ key = "DownArrow", mods = "ALT", action = act.Nop },
+
+		-- Word traversal
+		{ key = "LeftArrow", mods = "ALT", action = wezterm.action.SendKey({ key = "b", mods = "ALT" }) },
+		{ key = "RightArrow", mods = "ALT", action = wezterm.action.SendKey({ key = "f", mods = "ALT" }) },
 	},
 
-	window_decorations = 'INTEGRATED_BUTTONS | RESIZE',
-	window_close_confirmation = 'NeverPrompt',
-	audible_bell = 'Disabled',
+	window_decorations = "INTEGRATED_BUTTONS | RESIZE",
+	window_close_confirmation = "NeverPrompt",
+	audible_bell = "Disabled",
 	adjust_window_size_when_changing_font_size = false,
 	automatically_reload_config = true,
-  hyperlink_rules = hyperlink_rules,
+	hyperlink_rules = hyperlink_rules,
 }
 
-function get_if_valid_file(pane, uri)
-  local function strip_colon(name)
-    local colon_pos = name:find(":")
-    if colon_pos then
-      return name:sub(1, colon_pos - 1)
-    else
-      return name
-    end
-  end
+function get_if_valid_file(name, input_uri)
+	local function strip_colon(filename)
+		local colon_pos = filename:find(":")
+		if colon_pos then
+			return filename:sub(1, colon_pos - 1)
+		else
+			return filename
+		end
+	end
 
-  local function file_exists(filename)
-    local f = io.open(strip_colon(filename), "r")
-    if f ~= nil then
-      f:close()
-      return true
-    else
-      return false
-    end
-  end
+	local function file_exists(filename)
+		local f = io.open(strip_colon(filename), "r")
+		if f ~= nil then
+			f:close()
+			return true
+		else
+			return false
+		end
+	end
 
-  -- check for the matched absolute file path
-  if file_exists(uri) then
-    return uri
-  end
+	-- check for the matched absolute file path
+	if file_exists(name) then
+		return name
+	end
 end
 
 wezterm.on("open-uri", function(window, pane, uri)
-  local filename = get_if_valid_file(pane, uri)
+	local filename = get_if_valid_file(pane, uri)
 
-  if filename then
-    os.execute('code --goto ' .. filename)
-    
-    -- return false to prevent opening in a browser (later handlers do this on open-uri)
-    return false 
-  end
+	if filename then
+		os.execute("code --goto " .. filename)
+
+		-- return false to prevent opening in a browser (later handlers do this on open-uri)
+		return false
+	end
 end)
 
 return config
