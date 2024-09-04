fefbd9adca9dae3fbfba0d2734a2fa792a74055a
Collin Murch <collin@murch.net>
2024-09-03 21:56:43 -0500

    ayo

--- install_macos.sh
+++ install_macos.sh
@@ -2,11 +2,11 @@
 
 brew install stow
 brew install neovim
+brew install ripgrep # for vim telescope
+brew install lua-language-server
 brew install starship
 brew install bat
-brew install ripgrep fd # for vim telescope
 brew install zsh-syntax-highlighting
-brew install lua-language-server
 brew install zsh-autosuggestions
 
 brew install raycast --cask
