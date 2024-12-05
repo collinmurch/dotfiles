3dc0be5eff2c815b871d46e7824a08b600c14dd5
Collin Murch <collin@murch.net>
2024-12-04 19:41:43 -0600

    remove zellij

--- install_macos.sh
+++ install_macos.sh
@@ -1,15 +1,14 @@
 #!/bin/zsh
 
-brew install stow neovim bat starship                       # cli
+brew install stow neovim helix bat starship                 # cli
 brew install ripgrep lua-language-server                    # neovim
 brew install zsh-syntax-highlighting zsh-autosuggestions    # zsh
 
 brew install --cask font-jetbrains-mono
 
 brew install --cask raycast
-brew install --cask wezterm
+# brew install --cask ghostty
 brew install --cask visual-studio-code
 brew install --cask zed
 
-git config --global core.editor "nvim"
-git config --global merge.tool "nvimdiff3"
+git config --global core.editor "hx"
