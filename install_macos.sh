6d0bbc8a255e92986e845edff98bc9d462509bb1
Collin Murch <collin@murch.net>
2024-12-25 20:32:34 -0800

    submodule update

--- install_macos.sh
+++ install_macos.sh
@@ -1,14 +1,14 @@
 #!/bin/zsh
 
-brew install stow neovim helix bat starship                 # cli
-brew install ripgrep lua-language-server                    # neovim
+brew install stow neovim bat starship jq                    # cli
+brew install ripgrep lua-language-server`                   # neovim
 brew install zsh-syntax-highlighting zsh-autosuggestions    # zsh
 
 brew install --cask font-jetbrains-mono
 
 brew install --cask raycast
-# brew install --cask ghostty
+brew install --cask ghostty
 brew install --cask visual-studio-code
 brew install --cask zed
 
-git config --global core.editor "hx"
+git config --global core.editor "nvim"
