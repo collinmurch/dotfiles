951644d948f1420f3d5b4ccbd952fcf27b9a205c
Collin Murch <collin@murch.net>
2024-12-27 17:03:51 -0800

    berkely mono

--- install_macos.sh
+++ install_macos.sh
@@ -4,8 +4,6 @@ brew install stow neovim bat starship jq                    # cli
 brew install ripgrep lua-language-server`                   # neovim
 brew install zsh-syntax-highlighting zsh-autosuggestions    # zsh
 
-brew install --cask font-jetbrains-mono
-
 brew install --cask raycast
 brew install --cask ghostty
 brew install --cask visual-studio-code
