eb9331411412cc44331831c081d624be69015ff4
Collin Murch <collin@murch.net>
2025-01-03 22:39:58 -0800

    install updates

--- install_macos.sh
+++ install_macos.sh
@@ -1,11 +1,10 @@
 #!/bin/zsh
 
-brew install stow neovim bat starship                    	# cli
-brew install ripgrep lua-language-server                    # neovim
-brew install zsh-syntax-highlighting zsh-autosuggestions    # zsh
+brew install stow neovim bat starship
+brew install ripgrep
+brew install zsh-syntax-highlighting zsh-autosuggestions
 
 brew install --cask raycast
 brew install --cask ghostty
 brew install --cask visual-studio-code
 brew install --cask zed
-
