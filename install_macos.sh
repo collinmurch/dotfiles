25533049cabe9108dadeb367ad914fa04d4cffec
Collin Murch <collin@murch.net>
2025-06-25 23:07:12 -0500

    delta changes

--- install_macos.sh
+++ install_macos.sh
@@ -53,4 +53,8 @@ git config --global core.pager delta
 git config --global interactive.diffFilter 'delta --color-only'
 git config --global delta.features poimandres
 git config --global delta.navigate true
+git config --global diff.context 7
 git config --global merge.conflictStyle zdiff3
+git config --global delta.side-by-side true
+git config --global delta.hyperlinks true
+git config --global delta.colorMoved default
