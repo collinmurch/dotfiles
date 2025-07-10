77af36a7cb4b913fff0e75e6dcde267a83652ae5
Collin Murch <collin@murch.net>
2025-07-09 21:47:26 -0500

    update flake

--- pkg-list.nix
+++ pkg-list.nix
@@ -1,15 +1,22 @@
 { pkgs }:
-with pkgs; [
-  # core shells/tools
-  nushell  stow  neovim  helix  bat
-  ripgrep  starship  jq  delta
-
-  # languages
-  nil  nixd  uv
-
-  # zsh plugins
-  zsh-syntax-highlighting    zsh-autosuggestions
-
+let
+  # Package groups
+  coreTools = with pkgs; [
+    nushell stow neovim helix bat
+    ripgrep starship jq delta
+  ];
+  
+  devTools = with pkgs; [
+    nil nixd uv
+  ];
+  
+  zshPlugins = with pkgs; [
+    zsh-syntax-highlighting zsh-autosuggestions
+  ];
+  
+  basePackages = coreTools ++ devTools ++ zshPlugins;
+in
+basePackages ++ [
   # gui applications (require allowUnfree = true)
   # raycast  zed-editor  vscode
 ]
