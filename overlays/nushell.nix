dffbcc5291b9e795188214ec2a453dd29ba1044b
Collin Murch <collin@murch.net>
2025-07-30 23:31:27 -0500

    patch nushell instead of fork

--- overlays/nushell.nix
+++ overlays/nushell.nix
@@ -1,24 +1,18 @@
 self: super:
-let
-  src = super.fetchFromGitHub {
-    owner = "collinmurch";
-    repo  = "nushell";
-    rev   = "853ee847a82e3c1ea975486fe923beb48a2d4887";
-    hash  = "sha256-MTBOPCxn0GWwixMNhb9MvfJrBSkEMGXPVMcFbGb6AM8=";
-  };
-in
 {
-  nushell = super.rustPlatform.buildRustPackage {
-    pname   = "nushell";
-    version = "0.0.1";
-    inherit src;
-    cargoHash = "sha256-firVJQA9jDgve9BMSN7VEovvhrrUNPUuVlte3Xdh3JQ=";
-    cargoBuildFlags = [
-      "--no-default-features"
-      "--features=rustls-tls"
+  nushell = super.nushell.overrideAttrs (oldAttrs: {
+    cargoPatches = (oldAttrs.cargoPatches or []) ++ [
+      (super.writeText "reedline-fork.patch" ''
+        --- a/Cargo.toml
+        +++ b/Cargo.toml
+        @@ -280,0 +280,3 @@ nu-ansi-term = {git = "https://github.com/nushell/nu-ansi-term.git", branch = "
+         # Run all benchmarks with `cargo bench`
+         # Run individual benchmarks like `cargo bench -- <regex>` e.g. `cargo bench -- parse`
+         [[bench]]
+        +
+        +[patch."https://github.com/nushell/reedline"]
+        +reedline = { git = "https://github.com/collinmurch/reedline", branch = "main" }
+      '')
     ];
-    nativeBuildInputs = with super; [ pkg-config cmake ];
-    buildInputs       = [ ];
-    doCheck           = false;
-  };
+  });
 }
